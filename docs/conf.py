610c45e8104cf0f62ee887f700dcaf4256f7c59f
Robert Lieck <science@robert-lieck.com>
2021-11-11 09:51:11 +0100

    add API summary to docu

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ extensions = [
     'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinx.ext.autosummary',
 ]
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
