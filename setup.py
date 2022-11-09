2635d23c180b4a28006b2ac70ae3569cf0478626
Christoph Finkensiep <chfin@chfin.de>
2022-11-09 15:43:58 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="pitchtypes",
-    version="0.3.0",
+    version="0.4.0",
     author="Robert Lieck",
     author_email="robert.lieck@epfl.ch",
     description="musically meaningful pitch and interval types",
