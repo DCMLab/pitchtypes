b06b989bfd3c814c26e0f01f7b331513709aeeb5
Johannes Hentschel <johannes.hentschel@epfl.ch>
2024-08-08 15:47:50 +0200

    explicitly writes out the imports in __init__.py to avoid wildcard import

--- pitchtypes/__init__.py
+++ pitchtypes/__init__.py
@@ -1,9 +1,44 @@
 #  Copyright (c) 2020 Robert Lieck
 
-from .basetypes import *
-from .spelled import *
-from .spelled_array import *
-from .enharmonic import *
-from .logfreq import *
-from .harmonic import *
+from .basetypes import AbstractBase, Interval, Chromatic, Diatonic, Pitch, Converters
+from .spelled import (
+    Spelled,
+    SpelledPitch,
+    SpelledInterval,
+    SpelledPitchClass,
+    SpelledIntervalClass,
+)
+from .spelled_array import (
+    SpelledArray,
+    SpelledIntervalArray,
+    SpelledIntervalClassArray,
+    SpelledPitchArray,
+    SpelledPitchClassArray,
+    asi,
+    asic,
+    asp,
+    aspc,
+)
+from .enharmonic import (
+    Enharmonic,
+    EnharmonicPitch,
+    EnharmonicInterval,
+    EnharmonicPitchClass,
+    EnharmonicIntervalClass,
+)
+from .logfreq import (
+    LogFreq,
+    LogFreqPitch,
+    LogFreqInterval,
+    LogFreqPitchClass,
+    LogFreqIntervalClass,
+)
+from .harmonic import Harmonic, HarmonicInterval, HarmonicIntervalClass
+from .generic import (
+    Generic,
+    GenericPitch,
+    GenericInterval,
+    GenericPitchClass,
+    GenericIntervalClass,
+)
 from . import converters
